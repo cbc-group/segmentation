45a0b6ddf4ee75e61deddf6182c8e5c2cd9a1572
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-03-26 17:21:38 +0800

    Force load as numpy

--- segmentation/pipeline/tasks/read.py
+++ segmentation/pipeline/tasks/read.py
@@ -1,5 +1,6 @@
 from dask import delayed
 import dask.array as da
+import numpy as np
 
 
 @delayed
@@ -7,6 +8,6 @@ def read_h5(h5_path, internal_path="/"):
     import h5py
 
     with h5py.File(h5_path, "r") as h:
-        data = h[internal_path]
-    print(f'{h5_path}, {data.shape}')
-    return da.from_array(data, chunks=data.chunks)
+        data = np.array(h[internal_path])
+    print(f"{h5_path}, {data.shape}")
+    return da.from_array(data, chunks="auto")
