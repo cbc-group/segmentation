be178de135589d07aa965775a3815cbbac8b416f
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-04-06 17:22:21 +0800

    Refactor packing script for single use

--- segmentation/pack.py
+++ segmentation/pack.py
@@ -1,54 +1,100 @@
 """
 Pack raw data and labels into HDF5 format.
 """
+import glob
 import logging
 import os
 
-import coloredlogs
 import h5py
 import imageio
 import numpy as np
 import SimpleITK as sitk
 
+import datetime
 
 logger = logging.getLogger(__name__)
 
 
-logging.getLogger("tifffile").setLevel(logging.ERROR)
-coloredlogs.install(
-    level="DEBUG", fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
-)
+def load_file_by_ext(src_dir, ext: str, reader, desc="data", return_path=False):
+    paths = glob.glob(os.path.join(src_dir, f"*.{ext}"))
+    try:
+        if len(paths) > 1:
+            raise RuntimeError(f"too many {desc} sources")
+        path = paths[0]
+    except IndexError:
+        raise RuntimeError(f"unable to locate {desc}")
 
-# src_dir = "U:/Andy/20191210_ExM_kidney_10XolympusNA06_zp3_10x14_kb_R_Nkcc2_488_slice_8_1/nucleus_bin4"
-src_dir = "U:/Andy/20191210_ExM_kidney_10XolympusNA06_zp3_10x14_kb_R_Nkcc2_488_slice_8_1/tubule_bin4"
+    data = reader(path)
+    if return_path:
+        return path, data
+    else:
+        return data
 
-logger.info("load raw")
-raw = os.path.join(src_dir, "3-Pos_000_008_bin4.nrrd")
-raw = imageio.imread(raw)
 
-"""
-logger.info("load label")
-label = os.path.join(src_dir, "ground_truth.nii")
-label = sitk.ReadImage(label)
-label = sitk.GetArrayFromImage(label)
+def load_label(src_dir, ext="nii.gz"):
+    def reader(path):
+        data = sitk.ReadImage(path)
+        return sitk.GetArrayFromImage(data)
 
-assert raw.shape == label.shape
-logger.info(f"dataset shape {raw.shape}")
-"""
+    path, data = load_file_by_ext(src_dir, ext, reader, desc="label", return_path=True)
 
-logger.info("convert dataset format (full)")
-with h5py.File("full.h5", "w") as h:
-    h["raw"] = raw
-    h["label"] = np.zeros_like(raw, dtype=np.uint8) # label.astype(np.uint8)
+    logger.info(f'loading label from "{os.path.basename(path)}"')
+    return data
 
-"""
-logger.info("convert dataset format (train)")
-with h5py.File("train.h5", "w") as h:
-    h["raw"] = raw[:64, ...]
-    h["label"] = label[:64, ...].astype(np.uint8)
 
-logger.info("convert dataset format (val)")
-with h5py.File("val.h5", "w") as h:
-    h["raw"] = raw[64:, ...]
-    h["label"] = label[64:, ...].astype(np.uint8)
-"""
+def load_raw_data(src_dir, ext="nrrd"):
+    path, data = load_file_by_ext(
+        src_dir, ext, imageio.imread, desc="raw data", return_path=True
+    )
+
+    logger.info(f'loading raw from "{os.path.basename(path)}"')
+    return data
+
+
+def main(src_dir, dst_dir=None, no_label=False):
+    raw = load_raw_data(src_dir)
+    logger.info(f"dataset shape {raw.shape}")
+
+    if not no_label:
+        label = load_label(src_dir)
+        assert raw.shape == label.shape, "raw data and label should have the same shape"
+
+        if label.dtype != np.uint8:
+            logger.warning("force label to cast as uint8")
+            label = label.astype(np.uint8)
+
+    if dst_dir is None:
+        ts = datetime.datetime.now().timestamp()
+        ts = int(ts)
+        dst_dir = os.path.join(src_dir, f"packed_{ts}")
+    try:
+        os.makedirs(dst_dir)
+    except FileExistsError:
+        pass
+    logger.info(f'saving result to "{dst_dir}"')
+
+    # split along depth
+    ds = raw.shape[0] // 2
+
+    logger.info(".. training set")
+    path = os.path.join(dst_dir, "train.h5")
+    with h5py.File(path, "w") as h:
+        h["raw"] = raw[:ds, ...]
+        h["label"] = label[:ds, ...]
+
+    logger.info(".. validation set")
+    path = os.path.join(dst_dir, "val.h5")
+    with h5py.File(path, "w") as h:
+        h["raw"] = raw[ds:, ...]
+        h["label"] = label[ds:, ...]
+
+
+if __name__ == "__main__":
+    import coloredlogs
+
+    logging.getLogger("tifffile").setLevel(logging.ERROR)
+    coloredlogs.install(
+        level="DEBUG", fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
+    )
+
+    main(src_dir="U:/Andy/20200217_K8_nkcc2_568_10x15_z5um_1/glomerulus")
